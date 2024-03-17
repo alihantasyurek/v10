54d9593fa9288fd054b5f081eef7088384583412
Alihan Tasyurek <atasyure@k2m15s09.42kocaeli.com.tr>
2024-03-17 18:47:47 +0300

    fixed is builtin

--- shell/build/builtin.c
+++ shell/build/builtin.c
@@ -6,64 +6,48 @@
 /*   By: atasyure <atasyure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 05:20:31 by aatasyure         #+#    #+#             */
-/*   Updated: 2024/03/17 06:09:20 by atasyure         ###   ########.fr       */
+/*   Updated: 2024/03/17 18:46:15 by atasyure         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 #include <stdlib.h>
 
-static void make_lowercase(char *string)
+static void	make_lowercase(char *string)
 {
-	int c;
+	int	c;
 
 	c = 0;
-	while(string[c] != '\0')
+	while (string[c] != '\0')
 	{
-	if(string[c] >= 'A' && string[c] <= 'Z')
-		string[c] += 32;
-	c++;
+		if (string[c] >= 'A' && string[c] <= 'Z')
+			string[c] += 32;
+		c++;
 	}
 }
 
-static int check_case(char *string)
+int	execute_builtin_command(t_parse *parse, t_mini *m_mini)
 {
-	int c;
+	char	*temp;
+	int		len;
 
-	c = 0;
-	while(string[c] != '\0')
-	{
-	if(string[c] >= 'A' && string[c] <= 'Z')
-		return (1);
-	c++;
-	}
-	return (0);
-}
-
-int		execute_builtin_command(t_parse *parse, t_mini *m_mini)
-{
-	char *temp;
-
-	temp = NULL;
-	temp = parse->cmd;
-
-	if(check_case(parse->cmd))
-		make_lowercase(parse->cmd);
-
-	if ((ft_strcmp(parse->cmd, "echo") == 0))
-		return(exec_echo(parse, m_mini),1);
-	if (ft_strcmp(parse->cmd, "cd" ) == 0)
-		return(exec_cd(parse, m_mini),1);
-	if ((ft_strcmp(parse->cmd, "pwd") == 0))
-		return(exec_pwd(parse, m_mini),1);
-	if ((ft_strcmp(parse->cmd, "env") == 0))
-		return(exec_env(m_mini),1);
+	len = ft_strlen(parse->cmd) + 1;
+	temp = malloc(sizeof(char *) * len);
+	ft_strlcpy(temp, parse->cmd, len);
+	make_lowercase(temp);
+	if ((ft_strcmp(temp, "echo") == 0))
+		return (free(temp), exec_echo(parse, m_mini), 1);
+	if (ft_strcmp(temp, "cd" ) == 0)
+		return (free(temp), exec_cd(parse, m_mini), 1);
+	if ((ft_strcmp(temp, "pwd") == 0))
+		return (free(temp), exec_pwd(parse, m_mini), 1);
+	if ((ft_strcmp(temp, "env") == 0))
+		return (free(temp), exec_env(m_mini), 1);
+	free(temp);
 	if (ft_strcmp(parse->cmd, "export") == 0)
-		return(exec_export(parse, m_mini),1);
+		return (exec_export(parse, m_mini), 1);
 	if (ft_strcmp(parse->cmd, "unset") == 0)
-		return(exec_unset(parse, m_mini, 0, 0),1);
-
-	parse->cmd = temp;
+		return (exec_unset(parse, m_mini, 0, 0), 1);
 	return (0);
 }
 
